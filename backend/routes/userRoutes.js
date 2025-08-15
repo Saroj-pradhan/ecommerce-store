141ba628e7b58fcaecd07790a613571cbfe44efe
saroj <sarojpradhan7655@gmail.com>
2025-08-15 23:23:19 +0530

    Completed User Signup, Login Route

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const router = express.Router();
+//Register new user
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    let isExitingUser = await User.findOne({ email });
+    if (isExitingUser)
+      return res.status(400).json({ message: "User Already Exists" });
+    const user = new User({
+      name,
+      email,
+      password,
+    });
+
+    await user.save();
+
+    //jwt payload
+    const payload = { user: { id: user._id, role: user.role } };
+    //jwt signature
+    const token = jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "48h" },
+      (err, token) => {
+        if (err) throw err;
+        res.json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.error("Error in registration ", error);
+    res.status(500).send("Error in registration ", error);
+  }
+});
+
+
+// Login route for user
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.json({ message: "No user Found , Please Register" });
+    //  Password verificaion/matching
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid password" });
+
+    const payload = { user: { id: user._id, role: user.role } };
+    const token = await jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "48h",
+    });
+    res.json({
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      message: "Login Succesfully",
+      token,
+    });
+  } catch (error) {
+    console.log("Error", error);
+    res.status(505).send("Error in Login , Server Error", error);
+  }
+});
+
+
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,29 @@
 const connectDB = require("./config/db")
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
+
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+
+// .env varibale load into process.env
 const dotenv = require("dotenv");
 dotenv.config();
+
 app.use(express.json());
 app.use(cors());
+
 app.get("/",(req,res)=>{
     res.send("safff  kkkk")
 })
+//All api endpoint related to /user
+app.use("/user",userRoutes);
+
+//defining port no
 const port = process.env.PORT || 5000;
+
+// Connecting to mongo database
 connectDB();
+// listening to server at port 
 app.listen(port,()=>{
     console.log("server started at 3000")
 });
